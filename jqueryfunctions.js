2d74978b6dc4dba11041a03863d7ec2b3cf504bc
Vaidehi <vaidehi@Vaidehis-MacBook-Air.local>
2018-12-20 16:09:51 +0530

    some minor changes in html file.

--- jqueryfunctions.js
+++ jqueryfunctions.js
@@ -7,30 +7,17 @@ $(document).ready(function(){
         $("#frmComposeMail").addClass("closeMailBox");
     });
 
-    // $("#btnUnRead").click(function(){
-    //     if($('input[id="chkbx2"]').is(':checked'))
-    //     {
-    //         // $("#div2").addClass("mailBody unreadMailBg subjectRead arcIcon mailBodyUnRDate");
-    //         $("#div2").toggleClass("mailBody unreadMailBg subjectRead arcIcon mailBodyUnRDate");
-    //         // $("#div2").removeClass("mailBody mailBgOpa subjectRead archiveIcon mailBodyDate");
-    //     }
-    // });
+    $("#btnUnRead").click(function(){
+        $("input[type='checkbox']:checked").each(function(){
+            $("#rdDiv1").addClass('unreadMailBg').removeClass('mailBgOpa');
+        });
+    });
 
     // $("#btnRead").click(function(){
-    //     if($('input[id="chkbx1"]').is(':checked') || $('input[id="chkbx6"]').is(':checked'))
-    //     {
-    //         $("#div1").addClass("read").removeClass('unreadMailBg');
-    //         $(".subject").addClass("readSub").removeClass('subject');
-    //         // $("#div1").addClass("read").removeClass('unreadMailBg');
-    //         // $("#div6").addClass("mailBody mailBgOpa subjectRead archiveIcon mailBodyDate");
-    //     }
-    //     // else if($('input[id="chkbx1"]').is(':checked'))
-    //     // {
-    //     //     $("#div1").addClass("mailBody mailBgOpa subjectRead archiveIcon mailBodyDate");
-    //     // }
-    //     // else if($('input[id="chkbx6"]').is(':checked'))
-    //     // {
-    //     //     $("#div6").addClass("mailBody mailBgOpa subjectRead archiveIcon mailBodyDate");
-    //     // }
+    //     $("input[type='checkbox']:checked").each(function(){
+    //         $("#unrdDiv").addClass('mailBgOpa').removeClass('unreadMailBg');
+    //         $("#rdDiv").addClass('mailBgOpa').removeClass('unreadMailBg');
+    //         // $("div[name='allMailDiv']").addClass('mailBgOpa').removeClass('unreadMailBg');
+    //     });
     // });
 });
