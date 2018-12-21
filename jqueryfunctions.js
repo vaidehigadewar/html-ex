93d45016820f1294846c055e61d5a661bfbf60b5
Vaidehi <vaidehi@Vaidehis-MacBook-Air.local>
2018-12-21 18:22:11 +0530

    code includes read,unread and toggle.

--- jqueryfunctions.js
+++ jqueryfunctions.js
@@ -8,35 +8,14 @@ $(document).ready(function(){
     });
 
     $("#btnUnRead").click(function(){
-        // $(".mailInbox > div > input").addClass('unreadMailBg').removeClass('mailBgOpa');
-        // $("div.mailInbox > div > input[type='checkbox']:checked").addClass('unreadMailBg').removeClass('mailBgOpa');
-        // $("input[type='checkbox']:checked").each(function(){
-        //     $("#rdDiv1").addClass('unreadMailBg').removeClass('mailBgOpa');
-        // });
-        // $(".mailBgOpa").css({
-        //     opacity: '0.9',
-        //     fontWeight: 'bold'
-        // })
-        // $(".mailInbox input[type='checkbox']:checked").each(function(){
-        //     $(".mailBgOpa").addClass('unreadMailBg').removeClass('mailBgOpa');
-        // })
-        $('.mailInbox input').on('click', event => {
-            $(event.currentTarget).addClass('unreadMailBg');
-            
-            $(event.currentTarget).siblings().removeClass('mailBgOpa')
-            
-            $('.mailInbox').children().removeClass('mailBgOpa')
-          });
+        $(".mailBody input[type='checkbox']:checked").closest('.mailBody').removeClass('mailBgOpa').addClass('unreadMailBg');
     });
 
     $("#btnRead").click(function(){
-        // $("input[type='checkbox']:checked").each(function(){
-        //     $("#unrdDiv").addClass('mailBgOpa').removeClass('unreadMailBg');
-        //     $("#rdDiv").addClass('mailBgOpa').removeClass('unreadMailBg');
-        //     // $("div[name='allMailDiv']").addClass('mailBgOpa').removeClass('unreadMailBg');
-        // });
-        $("input[type='checkbox']:checked").each(function(){
-            $(".unreadMailBg").addClass('.mailBgOpa').removeClass('unreadMailBg');
-        })
+        $(".mailBody input[type='checkbox']:checked").closest('.mailBody').removeClass('unreadMailBg').addClass('mailBgOpa');
+    });
+
+    $("#btnToggle").click(function(){
+        $(".mailBody input[type='checkbox']:checked").closest('.mailBody').toggleClass('unreadMailBg');
     });
 });
