253cd7368d07ee873e87ad5ce814adf431ca1869
Vaidehi <vaidehi@Vaidehis-MacBook-Air.local>
2018-12-24 14:40:22 +0530

    changes made in query path.

--- jqueryfunctions.js
+++ jqueryfunctions.js
@@ -16,8 +16,11 @@ $(document).ready(function(){
     });
 
     $("#btnToggle").click(function(){
-        $(".mailBody input[type='checkbox']:checked").closest('.mailBody').toggleClass('unreadMailBg');
-
+        // $(".mailBody input[type='checkbox']:checked").closest('.mailBody').toggleClass('unreadMailBg');
+        $(".mailBody input[type='checkbox']:checked").closest('.mailBody').addClass('mailBgOpa').removeClass('unreadMailBg');
         $(".mailBody input[type='checkbox']:checked").prop('checked',false);
+        $(".mailBody input[type='checkbox']:checked").each(function(){
+            $(".mailBody input[type='checkbox']:checked").closest('.mailBody').removeClass('mailBgOpa').addClass('unreadMailBg');
+        })
     });
 });
